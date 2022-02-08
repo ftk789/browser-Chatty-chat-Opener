756941ad5c807dc46577d846cf346733561ee0c3
moath alayel <57598907+ftk789@users.noreply.github.com>
2022-02-08 23:31:49 +0200

    Update Chatty Chat Opener 2.0.user.js

--- Chatty Chat Opener 2.0.user.js
+++ Chatty Chat Opener 2.0.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name         Chatty Chat Opener 2.0
+// @author       ftk789
 // @namespace    http://tampermonkey.net/
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.min.jssli
 // @grant        GM_getValue
@@ -9,11 +10,46 @@
 // @version      0.1
 // @description  Opens chat in Chatty with a button
 // @author       @ftk789
-// @match        *://*.twitch.tv/*
-// @match        *://*.twitch.tv/moderator/*
+// @match        https://www.twitch.tv/*
+// @match        https://www.twitch.tv/moderator/*
 // @icon         https://img.utdstc.com/icon/a22/140/a2214007e685001a603b2645d4c05c260f94c7fb3d69175696d009ce3e9d27f5:200
 // @run-at       document-idle
+// @license      MIT
 // ==/UserScript==
+
+let url = window.location.href;
+
+['click','popstate', 'onload'].forEach( evt =>
+        window.addEventListener(evt, function () {
+            requestAnimationFrame(()=>{
+                if (url !== location.href) {
+                const myUrl = new URL(window.location.href);
+                parent.location='chatty:'+ myUrl.pathname;
+                        function addButton(ele) {
+       let btn = document.createElement("button");
+        btn.innerHTML = "Open in Chatty";
+        btn.className = "btn";
+        btn.onclick = () => {
+            const myUrl = new URL(window.location.href);
+            // alert("hi, my name is "+myUrl.pathname);
+            // console.log(myUrl.pathname);
+            parent.location='chatty:'+ myUrl.pathname;
+
+        }
+        ele.insertBefore(document.createElement('br'), ele.childNodes[8]);
+        ele.insertBefore(btn, ele.childNodes[8]);
+    }
+    let preTags = document.getElementsByTagName("h4");
+    let C = preTags[7];
+   for (let preTag of preTags ) {
+       addButton(preTag);
+   }
+                }
+                url = location.href;
+            });
+        }, true)
+    );
+
 
 (function() {
     'use strict';
